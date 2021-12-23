4cdf32e97251dc1b52531a7593cfd007a44699ee
Mr.L <admin@duxphp.com>
2021-12-23 16:09:53 +0800

    增加tag模块

---
+++ src/Cms/Model/CmsTags.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Cms\Model;
+
+/**
+ * class CmsTags
+ * @package Modules\Cms\Model
+ */
+class CmsTags extends \Duxravel\Core\Model\Base
+{
+
+    protected $table = 'cms_tags';
+
+    protected $primaryKey = 'tag_id';
+
+}
---
+++ src/Cms/Traits/Tags.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace Modules\Cms\Traits;
+
+use Modules\Cms\Model\CmsTags;
+
+/**
+ * Class RoleHas
+ * @package Duxravel\Core\Traits
+ */
+trait Tags
+{
+
+    /**
+     * 标签关联
+     * @return \Illuminate\Database\Eloquent\Relations\MorphToMany
+     */
+    public function tags()
+    {
+        return $this->morphToMany(CmsTags::class, 'has', 'cms_tags_has', 'has_id', 'tag_id');
+    }
+
+
+    /**
+     * 同步标签
+     * @param $tags
+     */
+    public function retag($tags)
+    {
+        $tags = $this->formatTags($tags);
+        // 获取已关联标签
+        $infoTags = $this->tags->pluck('name')->toArray();
+
+        $deletions = array_diff($infoTags, $tags);
+        $additions = array_diff($tags, $infoTags);
+
+        $this->untag($deletions);
+
+        foreach ($additions as $vo) {
+            $this->addTag($vo);
+        }
+
+    }
+
+    /**
+     * 删除标签
+     * @param array $tags
+     */
+    public function untag($tags = [])
+    {
+        if ($tags) {
+            $tags = $this->formatTags($tags);
+        }else {
+            $tags = $this->tags->pluck('name')->toArray();
+        }
+        foreach ($tags as $vo) {
+            $tagInfo = $this->tags()->where('name', $vo)->first();
+            // 移除关联
+            $this->tags()->detach($tagInfo->tag_id);
+            if ($tagInfo->count <= 1) {
+                // 剩余一个删除标签
+                $tagInfo->delete();
+            } else {
+                $tagInfo->decrement('count');
+            }
+        }
+    }
+
+    /**
+     * 增加单个标签
+     * @param $tag
+     */
+    private function addTag($tag) {
+        $tagName = trim($tag);
+
+        if(strlen($tagName) == 0) {
+            return;
+        }
+
+        $tagInfo = $this->tags()->where('name', $tagName)->first();
+        if ($tagInfo) {
+            // 递增引用次数
+            $tagInfo->increment('count');
+            $tagId = $tagInfo->tag_id;
+        } else {
+            // 创建应用
+            $tagInfo = new CmsTags;
+            $tagInfo->name = $tagName;
+            $tagInfo->count = 1;
+            $tagInfo->save();
+            $tagId = $tagInfo->tag_id;
+        }
+        $this->tags()->attach($tagId);
+    }
+
+
+    /**
+     * 格式化标签
+     * @param $tags
+     * @return array
+     */
+    private function formatTags($tags)
+    {
+        if (is_string($tags)) {
+            $tags = explode(',', $tags);
+        }
+
+        if (is_array($tags)) {
+            $tags = array_filter($tags);
+        }
+        $tags = array_map('trim', $tags);
+
+        return array_values($tags);
+    }
+
+
+}
