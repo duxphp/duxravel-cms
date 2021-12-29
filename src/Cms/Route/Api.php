956c5d50cb148e1ec14d461aa4285f71c39e2f98
Mr.L <admin@duxphp.com>
2021-12-29 16:02:09 +0800

    去除无用路由

--- src/Cms/Route/Api.php
+++ src/Cms/Route/Api.php
@@ -2,10 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-
-Route::get('form/{id}', ['uses' => 'Modules\Cms\Api\Form@list', 'desc' => '表单列表'])->name('api.cms.form.list');
-Route::get('form-info/{id}', ['uses' => 'Modules\Cms\Api\Form@info', 'desc' => '表单列表'])->name('api.cms.form.list');
-Route::post('form-info/{id}', ['uses' => 'Modules\Cms\Api\Form@push', 'desc' => '表单提交'])->name('api.cms.form.push');
-
-
 Route::get('CmsMenu/{id}', ['uses' => 'Modules\Cms\Api\Form@list', 'desc' => '表单列表'])->name('api.cms.form.list');
